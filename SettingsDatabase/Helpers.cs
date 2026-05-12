2524ff38ed1fb31a012d547d64986b1cd21680bc
Brandon <brandon.t@greentecauto.com>
2026-05-12 12:42:30 -0500

    Settings file is replaced by a settings db

---
+++ SettingsDatabase/Helpers.cs
@@ -0,0 +1,157 @@
+﻿namespace DbContext.SettingsDatabase
+{
+    public class Helpers
+    {
+        public static List<string> GetBuildOrderFromPrefix(string prefix)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                var order = db.build_orders.Where(b => b.Prefix == prefix)
+                    .OrderBy(b => b.OrderId)
+                    .Select(b => b.ModuleType)
+                    .ToList();
+                return order;
+            }
+        }
+        public static Dictionary<string, double> GetIrMultipliers(string model)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.ir_multipliers.Where(m => m.Model == model)
+                    .ToDictionary(m => m.ModuleType, m => m.Multiplier);
+            }
+        }
+        public static List<string> GetAllModulePrefixes()
+        {
+            using (var db = new SettingsDbContext())
+            {
+                List<string> models = db.models.Where(r => r.ModuleOrBattery == "Module").Select(m => m.Model).ToList();
+                return db.prefixes.Where(p => models.Contains(p.Model)).Select(p => p.Prefix).ToList();
+            }
+        }
+        public static string? GetEditPassword()
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.misc_settings.FirstOrDefault(r => r.Field1 == "EditPassword")?.Field2;
+            }
+        }
+        public static List<string> GetAllBatteryModels()
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.models.Where(m => m.ModuleOrBattery == "Battery").Select(m => m.Model).ToList();
+            }
+        }
+
+        public static List<string> GetAllDistinctTypes(string prefix)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.build_orders.Where(b => b.Prefix == prefix)
+                    .Select(b => b.ModuleType)
+                    .Distinct()
+                    .ToList();
+            }
+        }
+
+        public static List<string> GetAllModuleModels()
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.models.Where(m => m.ModuleOrBattery == "Module").Select(m => m.Model).ToList();
+            }
+        }
+
+        public static ModuleSettingRow? GetModuleSettings(string model)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.module_settings.FirstOrDefault(m => m.Model == model) ?? null;
+            }
+        }
+
+        public static Dictionary<string, double> GetIRMultipliers(string model)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.ir_multipliers.Where(m => m.Model == model)
+                    .ToDictionary(m => m.ModuleType, m => m.Multiplier);
+            }
+        }
+
+        public static List<string> GetPrefixes(string model)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.prefixes.Where(p => p.Model == model)
+                    .Select(p => p.Prefix)
+                    .ToList();
+            }
+        }
+
+        public static (string?, string?) GetModelFromSerial(string barcode)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                var prefix = db.prefixes.Where(p => barcode.StartsWith(p.Prefix)).FirstOrDefault();
+                if (prefix == null) return (null, null);
+                return (prefix.Prefix, prefix.Model);
+            }
+        }
+
+        public static string? GetModelFromPrefix(string prefix)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.prefixes.FirstOrDefault(p => p.Prefix == prefix)?.Model;
+            }
+        }
+
+        public static BatterySettingRow? GetBatterySetting(string model)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.battery_settings.FirstOrDefault(b => b.Model == model);
+            }
+        }
+
+        public static List<string> GetBatteryTypesFromModel(string model)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.battery_limits.Where(b => b.Model == model).Select(b => b.BatteryType).ToList();
+            }
+        }
+
+        public static BatteryLimitRow? GetBatteryLimit(string model, string type)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.battery_limits.FirstOrDefault(b => b.Model == model && b.BatteryType == type);
+            }
+        }
+
+        public static List<string> GetBuildOrder(string prefix)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                var order = db.build_orders.Where(b => b.Prefix == prefix)
+                    .OrderBy(b => b.OrderId)
+                    .Select(b => b.ModuleType)
+                    .ToList();
+                return order;
+            }
+        }
+
+        public static Dictionary<string, int> GetTypesPerPrefix(string prefix)
+        {
+            using (var db = new SettingsDbContext())
+            {
+                return db.build_orders.Where(b => b.Prefix == prefix)
+                    .GroupBy(b => b.ModuleType)
+                    .ToDictionary(g => g.Key, g => g.Count());
+            }
+        }
+    }
+}
---
+++ SettingsDatabase/SettingsDbContext.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbContext.SettingsDatabase
+{
+    public class SettingsDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        private readonly string _connString;
+        public SettingsDbContext()
+        {
+            _connString =
+                "Server=192.168.50.200;" +
+                "Port=3306;" +
+                "Database=settings;" +
+                "User Id=admin;" +
+                "Password=admin;";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseMySql(_connString, ServerVersion.AutoDetect(_connString));
+
+        public DbSet<ModelRow> models { get; set; }
+        public DbSet<PrefixRow> prefixes { get; set; }
+        public DbSet<IRMultiplierRow> ir_multipliers { get; set; }
+        public DbSet<ModuleSettingRow> module_settings { get; set; }
+        public DbSet<VoltageDataSourceRow> voltage_data_sources { get; set; }
+        public DbSet<BatterySettingRow> battery_settings { get; set; }
+        public DbSet<BatteryLimitRow> battery_limits { get; set; }
+        public DbSet<BuildOrderRow> build_orders { get; set; }
+        public DbSet<MiscRow> misc_settings { get; set; }
+    }
+
+
+    public class MiscRow
+    {
+        [Key]
+        public int id { get; set; }
+        public string Field1 { get; set; } = "";
+        public string Field2 { get; set; } = "";
+    }
+
+    public class ModelRow
+    {
+        [Key]
+        public string Model { get; set; } = "";
+        public string ModuleOrBattery { get; set; } = "";
+    }
+
+    public class PrefixRow
+    {
+        [Key]
+        public string Prefix { get; set; } = "";
+        public string Model { get; set; } = "";
+    }
+
+    [PrimaryKey(nameof(Model), nameof(ModuleType))]
+    public class IRMultiplierRow
+    {
+        public string Model { get; set; } = "";
+        public string ModuleType { get; set; } = "";
+        public double Multiplier { get; set; }
+    }
+
+    public class ModuleSettingRow
+    {
+        [Key]
+        public string Model { get; set; } = "";
+        public double? CapacityLowerLimit { get; set; }
+        public double? IRUpperLimit { get; set; }
+        public string VoltageDataSource { get; set; } = "";
+        public double CutoffVoltage { get; set; }
+        public double ClusterCutoffVoltage { get; set; }
+        public double DeltaVLimit { get; set; }
+        public double DeltaIRLimit { get; set; }
+        public double DeltaCapLimit { get; set; }
+        public int MinTestLength { get; set; }
+        public int IRWeight { get; set; }
+        public int CapWeight { get; set; }
+        public int VoltWeight { get; set; }
+    }
+
+    public class BatterySettingRow
+    {
+        [Key]
+        public string Model { get; set; } = "";
+        public string VoltageDataSource { get; set; } = "";
+        public int MinTestLength { get; set; }
+    }
+
+    public class BuildOrderRow
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public string Prefix { get; set; } = "";
+        public string ModuleType { get; set; } = "";
+    }
+
+    public class BatteryLimitRow
+    {
+        [Key]
+        public string BatteryType { get; set; } = "";
+        public double DVLimit { get; set; }
+        public double CapLimit { get; set; }
+        public string Model { get; set; } = "";
+    }
+
+    public class VoltageDataSourceRow
+    {
+        [Key]
+        public string VoltageDataSource { get; set; } = "";
+    }
+}
